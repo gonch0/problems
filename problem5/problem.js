b14196f64f042ee91651c32620a41cffffe95dcd
ivan <givan1010@gmail.com>
2021-07-03 21:18:24 +0500

    Problem5

---
+++ problem5/problem.js
@@ -0,0 +1,17 @@
+var i = 10;
+var array = [];
+
+while (i--) {
+    console.log('counter inside loop = ', i);
+
+    array.push(function closureExample() {
+        return i + i;
+    });
+
+}
+
+console.log('counter after loop', i);
+
+console.log([array[0]()]); // -2
+console.log([array[1]()]); // -2
+
