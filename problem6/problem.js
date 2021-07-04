cf6f339f3786e557c1e4675b5701b543b7031158
ivan <givan1010@gmail.com>
2021-07-04 18:18:45 +0500

    Problem6 fix

--- problem6/problem.js
+++ problem6/problem.js
@@ -16,19 +16,82 @@ function baz(callback) {
     }, Math.random()*100);
 }
 
-// Решение 1
+// Решение 2
 
-new Promise(function(resolve) {
-    // resolve()
-    setTimeout(() => resolve(foo(console.log)), 100);
+ new Promise((resolve) => {
+    foo(resolve);
+}).then((data1) => {
+    console.log(data1);
 
-}).then(function () {
-    return new Promise(function (resolve) {
-        setTimeout(() => resolve(bar(console.log)), 100);
-    })
+    return new Promise((resolve) => {
+        bar(resolve);
 
-}).then(function()  {
-    return new Promise(function (resolve) {
-        setTimeout(() => resolve(baz(console.log)), 100);
+    }).then((data2) => {
+        console.log(data2);
+            return new Promise((resolve) => {
+                baz(resolve);
+            }).then((data3) => {
+                console.log(data3)
+            })
     })
 })
+
+
+// Promise.resolve([p1])
+
+
+//
+// const p = new Promise((resolve) => {
+//     foo(resolve);
+// })
+//
+// p.then(() => {
+//     foo(console.log);
+//     return new Promise(resolve => {
+//         bar(resolve);
+//     }).then(() => {
+//         bar(console.log);
+//         return new Promise(resolve => {
+//             baz(resolve);
+//         }).then(() => {
+//             baz(console.log)
+//         })
+//
+//     })
+// })
+
+
+
+
+
+
+// Решение 1 (не решение)
+
+// new Promise(function(resolve) {
+//     // resolve()
+//     setTimeout(() => resolve(foo(console.log)), 100);
+//
+// }).then(function () {
+//     return new Promise(function (resolve) {
+//         setTimeout(() => resolve(bar(console.log)), 100);
+//     })
+//
+// }).then(function()  {
+//     return new Promise(function (resolve) {
+//         setTimeout(() => resolve(baz(console.log)), 100);
+//     })
+// })
+
+// Решение 1.2
+
+// setTimeout(() => {
+//     foo(console.log)
+//     setTimeout(() => {
+//         bar(console.log)
+//         setTimeout(() => {
+//             baz(console.log)}, 100);
+//     }, 100)
+// }, 100);
+
+
+
